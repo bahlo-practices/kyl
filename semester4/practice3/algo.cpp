5f3b5e3332ade2f8337836c391821fa91af654b6
Arne Bahlo <hallo@arne.me>
2014-04-28 13:11:07 +0200

    Add substituteString()

--- semester4/practice3/algo.cpp
+++ semester4/practice3/algo.cpp
@@ -3,33 +3,47 @@
 
 using namespace std;
 
-int substitute(int value) {
+char substituteChar(char value) {
   // The S-Box
-  int s_Box[8192];
+  size_t size = 128;
+  int sBox[size];
 
-  // Populate s_Box with distinct values
+  // Populate sBox with distinct values
   bool exists = false;
   int counter = 0;
   int tmp = 0;
-  while (counter < 8192) {
-      tmp = (int) rand() % 8192;
+  while (counter < size) {
+      tmp = (int) rand() % size;
       exists = false;
-      for (int i = 0; i < 8191; ++i) {
-          if (s_Box[i] == tmp) {
+      for (int i = 0; i < size - 1; ++i) {
+          if (sBox[i] == tmp) {
               exists = true;
               break;
           }
       }
       if (!exists) {
-          s_Box[counter] = tmp;
+          sBox[counter] = tmp;
           counter++;
       }
   }
 
   // Get the substitution
-  return s_Box[value];
+  return (char) sBox[(int) value];
+}
+
+string substituteString(string value) {
+  string result;
+  char tmp;
+
+  for (size_t i = 0; i < value.size(); ++i) {
+    tmp = substituteChar(value[i]);
+    result.append(&tmp);
+  }
+
+  return result;
 }
 
 int main() {
-  std::cout << substitute(20) << std::endl;
+  std::cout << substituteChar('b') << std::endl;
+  std::cout << substituteString("Test") << std::endl;
 }
